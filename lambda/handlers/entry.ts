47f1a7aca18b2b339fc493fe608db8105244e66e
Yoshida <ykhr53@yokohei.com>
2021-08-29 19:42:52 +0900

    Add slack challenge response

--- lambda/handlers/entry.ts
+++ lambda/handlers/entry.ts
@@ -1,5 +1,23 @@
 exports.handler = async function (event: any, context: any) {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+    let lambdaEvent;
+    if (event.body) {
+        lambdaEvent = JSON.parse(event.body);
+    }
+
+    // Handle challenge request
+    const challenge = lambdaEvent.challenge;
+    if (challenge) {
+        const body = {
+            challenge: challenge,
+        };
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(body),
+        };
+        return response;
+    }
+
     const response = {
         statusCode: 200,
         headers: {},
