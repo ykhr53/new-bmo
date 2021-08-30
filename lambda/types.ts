b82817ad12ba0312e59f6292416a4e2800a60038
suzutomo <sztmsk@gmail.com>
2021-08-30 10:07:42 +0900

    add lambda request type

--- lambda/types.ts
+++ lambda/types.ts
@@ -1,3 +1,23 @@
+export interface LambdaRequest {
+    resource: string;
+    path: string;
+    httpMethod: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+    headers: {
+        [key: string]: string;
+    } | null;
+    multiValueHeaders: {
+        [key: string]: string[];
+    } | null;
+    queryStringParameters: {
+        [key: string]: string;
+    } | null;
+    multiValueQueryStringParameters: {
+        [key: string]: string[];
+    } | null;
+    body: string | null;
+    isBase64Encoded: boolean;
+}
+
 export interface LambdaResponse {
     isBase64Encoded?: boolean;
     statusCode: number;
